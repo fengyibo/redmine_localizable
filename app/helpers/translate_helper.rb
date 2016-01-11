940d9fe66df92e59b579436784e678c5c9420938
Felipe Machado <felipe.machado@visagio.com>
2016-01-11 13:08:46 -0200

    removing other plugin dependency

--- app/helpers/translate_helper.rb
+++ app/helpers/translate_helper.rb
@@ -1,11 +1,11 @@
 module TranslateHelper
-  def translate(original_value, type)
+  def translate(value, type)
     if Setting["plugin_localizable"]
       if Setting["plugin_localizable"]["locales"]
         if Setting["plugin_localizable"]["locales"][type]
-          if Setting["plugin_localizable"]["locales"][type][original_value]
-            unless Setting["plugin_localizable"]["locales"][type][original_value][User.current.language.to_s].blank?
-              value = Setting["plugin_localizable"]["locales"][type][original_value][User.current.language.to_s]
+          if Setting["plugin_localizable"]["locales"][type][value]
+            unless Setting["plugin_localizable"]["locales"][type][value][User.current.language.to_s].blank?
+              value = Setting["plugin_localizable"]["locales"][type][value][User.current.language.to_s]
             end
           end
         end
