a6bc2ec810a112cf41ae6463afe6566df67edcf6
vinicius.garcia <vinicius.garcia@visagio.com>
2018-02-06 19:50:19 -0200

    'show_value' method now calls super - Now, other plugins can make their own additions to 'show_value' method.

--- lib/localizable/patches/custom_fields_helper_patch.rb
+++ lib/localizable/patches/custom_fields_helper_patch.rb
@@ -13,10 +13,11 @@ module Localizable
 
       module InstanceMethods
         include TranslateHelper
+
         # Return a string used to display a custom value
         def show_value_with_localizable(custom_value, html=true)
           custom_value.custom_field.default_value = translate_value(custom_value.custom_field.default_value, :defaults)
-          format_object(custom_value, html)
+          show_value_without_localizable(custom_value, html)
         end
       end
     end
